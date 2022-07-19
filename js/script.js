2ecf3e86581e6e63655a0d478945cc640ea13f44
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-19 22:41:16 +0600

    code done

--- js/script.js
+++ js/script.js
@@ -23,7 +23,52 @@ searchButton.addEventListener(`click`, () => {
       result.innerHTML = `
        
        <img src="${data[0].flags.svg}" class="flag-img">
-       
+       <h2>${data[0].name.common}
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+       </div>
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+       </div>
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+       </div>
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)].name
+                } - ${Object.keys(data[0].currencies)[0]} </span>
+            </div>
+       </div>
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(",")} </span>
+            </div>
+       </div>
        `;
-    });
+    }).catch(() => {
+      if (countryName.length == 0) {
+        result.innerHTML = `
+        <h3>The input field cannot be empty.</h3>
+        `
+      } else {
+        result.innerHTML = `
+        <h3>Please enter a valid country name.</h3>
+        `
+      }
+    })
 });

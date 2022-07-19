9be2c6355f408126d4e56922eb72c0ac448314c6
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-20 02:06:03 +0600

    code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 let searchButton = document.getElementById(`search-button`);
-
 let countryInput = document.getElementById(`country-input`);
 
 searchButton.addEventListener(`click`, () => {
@@ -10,20 +9,17 @@ searchButton.addEventListener(`click`, () => {
   fetch(finalUrl)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data[0]);
-      console.log(data[0].capital[0]);
-      console.log(data[0].flags.svg);
-      console.log(data[0].name.common);
-      console.log(data[0].continents[0]);
-      console.log(Object.keys(data[0].currencies)[0]);
-      console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
-      console.log(
-        Object.values(data[0].languages).toString().split(",").join(",")
-      );
+      console.log(data);
       result.innerHTML = `
        
        <img src="${data[0].flags.svg}" class="flag-img">
-       <h2>${data[0].name.common}
+       <h2>${data[0].name.common}</h2>
+       <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Official Name:</h4>
+                <span>${data[0].name.official}</span>
+            </div>
+       </div>
        <div class="wrapper">
             <div class="data-wrapper">
                 <h4>Capital:</h4>
@@ -60,15 +56,16 @@ searchButton.addEventListener(`click`, () => {
             </div>
        </div>
        `;
-    }).catch(() => {
+    })
+    .catch(() => {
       if (countryName.length == 0) {
         result.innerHTML = `
         <h3>The input field cannot be empty.</h3>
-        `
+        `;
       } else {
         result.innerHTML = `
         <h3>Please enter a valid country name.</h3>
-        `
+        `;
       }
-    })
+    });
 });
